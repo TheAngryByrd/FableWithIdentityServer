7cc423b27269e912cbf6a6aff8d49b2ab8a0dc1e
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2017-06-01 22:03:15 -0400

    Added server basic starts

--- src/AppServer/Program.fs
+++ src/AppServer/Program.fs
@@ -1,5 +1,47 @@
-namespace FableWithIdentityServer
+namespace AppServer
 
-module Say =
-    let hello name =
-        sprintf "Hello %s" name 
+
+open System
+open System.IO
+open System.Collections.Generic
+open System.Threading
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Http.Features
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open Giraffe.HttpContextExtensions
+open Giraffe.HttpHandlers
+open Giraffe.Middleware
+
+
+module Main =
+
+
+    let webApp = 
+        choose [
+            subRoute "/api"(
+                GET >=>
+                    choose [
+                        route "/foo" >=> text "foo"
+                    ]
+            )
+        ]
+
+
+    let configureApp (app : IApplicationBuilder) = 
+    //     app.UseGiraffeErrorHandler errorHandler
+    //     app.UseCookieAuthentication cookieAuth |> ignore
+    //     app.UseStaticFiles() |> ignore
+        app.UseGiraffe webApp
+    [<EntryPoint>]
+    let main args =
+        let url = "http://localhost:8085"
+        WebHostBuilder()
+            .UseUrls(url)
+            .UseKestrel()
+            .Configure(fun app -> app |> configureApp)
+            .Build()
+            .Run()
+        0
--- src/IdentityServer/Program.fs
+++ src/IdentityServer/Program.fs
@@ -1,5 +1,57 @@
-namespace FableWithIdentityServer
+namespace IdentityServer
 
-module Say =
-    let hello name =
-        sprintf "Hello %s" name 
+
+
+open System
+open System.IO
+open System.Collections.Generic
+open System.Threading
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Http.Features
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open IdentityServer4
+open IdentityServer4.Models
+
+module Config =
+    let getApiResources () =
+        [
+            ApiResource("api","my api")
+        ]
+
+    let getClients () =
+        [
+            Client(
+                ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = ResizeArray<_> [ Secret("secret".Sha256()) ],
+                AllowedScopes = ResizeArray<_> ["api"])
+        ]
+module Main =
+
+    let configureServices (services : IServiceCollection) =
+        services
+            .AddIdentityServer()
+            .AddTemporarySigningCredential()
+            .AddInMemoryApiResources(Config.getApiResources())
+            .AddInMemoryClients(Config.getClients())
+            |> ignore
+    let configureApp (app : IApplicationBuilder) = 
+    
+        app.UseIdentityServer() |> ignore
+        ()
+        
+
+    [<EntryPoint>]
+    let main args =
+        let url = "http://localhost:8123"
+        WebHostBuilder()
+            .UseUrls(url)
+            .UseKestrel()
+            .Configure(fun app -> app |> configureApp)
+            .ConfigureServices(fun s -> s |> configureServices)
+            .Build()
+            .Run()
+        0
